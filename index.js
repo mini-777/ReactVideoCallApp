ab3e8d4c5206d5dd4cd00e84ed632f1ee5aea197
mini-777 <pho0570@gmail.com>
2021-01-26 12:01:31 +0900

    update alert

--- index.js
+++ index.js
@@ -2,22 +2,14 @@
  * @format
  */
 
+// index.js
 import {AppRegistry} from 'react-native';
-import App from './App';
 import messaging from '@react-native-firebase/messaging';
-import {name as appName} from './app.json';
+import App from './App';
 
+// Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
 
-function HeadlessCheck({isHeadless}) {
-  if (isHeadless) {
-    // App has been launched in the background by iOS, ignore
-    return null;
-  }
-
-  return <App />;
-}
-
-AppRegistry.registerComponent('app', () => HeadlessCheck);
+AppRegistry.registerComponent('app', () => App);
