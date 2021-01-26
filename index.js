d4ab150b7b8048ab3670238674068e0abd755b8b
mini-777 <pho0570@gmail.com>
2021-01-26 10:20:42 +0900

    update index.js background handling

--- index.js
+++ index.js
@@ -4,11 +4,20 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-// import messaging from '@react-native-firebase/messaging';
+import messaging from '@react-native-firebase/messaging';
 import {name as appName} from './app.json';
 
-// messaging().setBackgroundMessageHandler(async remoteMessage => {
-//   console.log('Message handled in the background!', remoteMessage);
-// });
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
-AppRegistry.registerComponent(appName, () => App);
+function HeadlessCheck({isHeadless}) {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null;
+  }
+
+  return <App />;
+}
+
+AppRegistry.registerComponent('app', () => HeadlessCheck);
