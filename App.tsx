7f23ce3c3fe11d758f4dc1c2de4e1fd13a305fd1
mini-777 <pho0570@gmail.com>
2021-02-04 16:20:23 +0900

    add Splash.tsx

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import Videocall from './src/screens/Videocall';
 import Signup from './src/screens/Signup';
 import Start from './src/screens/Start';
 import Vendor from './src/screens/Vendor';
+import Splash from './src/screens/Splash';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import messaging from '@react-native-firebase/messaging';
@@ -26,57 +27,33 @@ function App() {
       console.log('Authorization status:', authStatus);
     }
   }
-  // useEffect(() => {
-  //   _checkPermission();
-  //   _listenForNotifications();
-
-  //   return function cleanup() {
-  //     notificationOpenedListener();
-  //   };
-  // });
-  // const _checkPermission = async () => {
-  //   const enabled = await messaging().hasPermission();
-  //   if (enabled) {
-  //     // user has permissions
-  //     console.log('FCM Permission Success');
-  //     // this._updateTokenToServer();
-  //   } else {
-  //     // user doesn't have permission
-  //     _requestPermission();
-  //   }
-  // };
-
-  // const _requestPermission = async () => {
-  //   try {
-  //     // User has authorised
-  //     await messaging().requestPermission();
-  //     // await this._updateTokenToServer();
-  //   } catch (error) {
-  //     // User has rejected permissions
-  //     Alert.alert("you can't handle push notification");
-  //   }
-  // };
-
-  // const notificationOpenedListener = messaging().onNotificationOpenedApp(
-  //   notificationOpen => {
-  //     console.log('onNotificationOpened', notificationOpen);
-  //   },
-  // );
-
-  // const _listenForNotifications = async () => {
-  //   messaging().onNotificationOpenedApp(notificationOpen => {
-  //     console.log('onNotificationOpened', notificationOpen);
-  //   });
-
-  //   const notificationOpen = await messaging().getInitialNotification();
-  //   if (notificationOpen) {
-  //     console.log('getInitialNotification', notificationOpen);
-  //     navigation.navigate('Vendor');
-  //   }
-  // };
 
   const Stack = createStackNavigator();
-
+  const Auth = () => {
+    // Stack Navigator for Login and Sign up Screen
+    return (
+      <Stack.Navigator initialRouteName="AuthScreen">
+        <Stack.Screen
+          name="Start"
+          component={Start}
+          options={{
+            title: '시작하기',
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{title: '로그인'}}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{title: '회원가입'}}
+        />
+      </Stack.Navigator>
+    );
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -91,34 +68,18 @@ function App() {
           },
         }}>
         <Stack.Screen
-          name="Start"
+          name="Splash"
           component={Start}
           options={{
-            title: '시작하기',
+            title: 'Splash',
             headerShown: false,
-            // headerStyle: {
-            //   backgroundColor: 'rgba(21,31,40,1)',
-            // },
-            // headerTintColor: 'rgba(21,31,40,1)',
-            // headerTitleStyle: {
-            //   fontWeight: 'bold',
-            // },
           }}
         />
+
         <Stack.Screen
           name="Contact"
           component={Contact}
           options={{title: '문의하기'}}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{title: '로그인'}}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{title: '회원가입'}}
         />
         <Stack.Screen name="Main" component={Main} options={{title: '메인'}} />
         <Stack.Screen
@@ -137,28 +98,3 @@ function App() {
 }
 
 export default App;
-
-// async _updateTokenToServer() {
-//   const fcmToken = await messaging().getToken();
-//   console.log(fcmToken);
-
-//   const header = {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//       Cache: 'no-cache',
-//     },
-//     body: JSON.stringify({
-//       user_id: 'CURRENT_USER_ID',
-//       firebase_token: fcmToken,
-//     }),
-//     credentials: 'include',
-//   };
-//   const url = 'http://YOUR_SERVER_URL';
-
-//   // if you want to notification using server,
-//   // do registry current user token
-
-//   // await fetch(url, header);
-// }
