1a320a675c83edf66c3eaea34ddc5bc2db500980
mini-777 <pho0570@gmail.com>
2021-02-17 15:09:10 +0900

    updated Nmap finish !!!

--- src/screens/Location.tsx
+++ src/screens/Location.tsx
@@ -1,122 +1,30 @@
+import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
-import NaverMapView, {
-  Circle,
-  Marker,
-  Path,
-  Polyline,
-  Polygon,
-  Align,
-} from './map';
-import {
-  PermissionsAndroid,
-  Platform,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import NaverMapView, {Marker} from '../map';
+import {PermissionsAndroid, Platform} from 'react-native';
 
-const P0 = {latitude: 37.564362, longitude: 126.977011};
-const P1 = {latitude: 37.565051, longitude: 126.978567};
-const P2 = {latitude: 37.565383, longitude: 126.976292};
 const P4 = {latitude: 37.564834, longitude: 126.977218};
 
-const Location = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="home" component={HomeScreen} />
-        <Stack.Screen name="stack" component={MapViewScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const HomeScreen = () => (
-  <Tab.Navigator>
-    <Tab.Screen name={'map'} component={MapViewScreen} />
-    <Tab.Screen name={'text'} component={TextScreen} />
-  </Tab.Navigator>
-);
-
-const TextScreen = () => {
-  return <Text>text</Text>;
-};
-
-const MapViewScreen = ({navigation}) => {
+const Location = ({navigation}) => {
   useEffect(() => {
     requestLocationPermission();
   }, []);
 
   return (
-    <>
-      <NaverMapView
-        style={{width: '100%', height: '100%'}}
-        showsMyLocationButton={true}
-        center={{...P0, zoom: 16}}
-        onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-        onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-        onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
-        useTextureView>
-        <Marker
-          coordinate={P0}
-          onClick={() => console.warn('onClick! p0')}
-          caption={{text: 'test caption', align: Align.Left}}
-        />
-        <Marker
-          coordinate={P1}
-          pinColor="blue"
-          onClick={() => console.warn('onClick! p1')}
-        />
-        <Marker
-          coordinate={P2}
-          pinColor="red"
-          onClick={() => console.warn('onClick! p2')}
-        />
-        <Marker
-          coordinate={P4}
-          onClick={() => console.warn('onClick! p4')}
-          image={require('./marker.png')}
-          width={48}
-          height={48}
-        />
-        <Path
-          coordinates={[P0, P1]}
-          onClick={() => console.warn('onClick! path')}
-          width={10}
-        />
-        <Polyline
-          coordinates={[P1, P2]}
-          onClick={() => console.warn('onClick! polyline')}
-        />
-        <Circle
-          coordinate={P0}
-          color={'rgba(255,0,0,0.3)'}
-          radius={200}
-          onClick={() => console.warn('onClick! circle')}
-        />
-        <Polygon
-          coordinates={[P0, P1, P2]}
-          color={`rgba(0, 0, 0, 0.5)`}
-          onClick={() => console.warn('onClick! polygon')}
-        />
-      </NaverMapView>
-      <TouchableOpacity
-        style={{position: 'absolute', bottom: '10%', right: 8}}
-        onPress={() => navigation.navigate('stack')}>
-        <View style={{backgroundColor: 'gray', padding: 4}}>
-          <Text style={{color: 'white'}}>open stack</Text>
-        </View>
-      </TouchableOpacity>
-      <Text
-        style={{
-          position: 'absolute',
-          top: '95%',
-          width: '100%',
-          textAlign: 'center',
-        }}>
-        Icon made by Pixel perfect from www.flaticon.com
-      </Text>
-    </>
+    <NaverMapView
+      style={{width: '100%', height: '100%'}}
+      center={{...P4, zoom: 16}}
+      onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+      onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+      onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
+      <Marker
+        coordinate={P4}
+        onClick={() => console.warn('onClick! p4')}
+        image={require('../img/marker.png')}
+        width={48}
+        height={48}
+      />
+    </NaverMapView>
   );
 };
 
